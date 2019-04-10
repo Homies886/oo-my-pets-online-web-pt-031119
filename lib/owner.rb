ae010e8538cf85aad0d269ef0a654015fc7b89ca
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-10 16:53:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -35,9 +35,11 @@ class Owner
   
   def buy_dog(name)
     new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
   end
   
   def buy_fish(name)
     new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
   end  
 end
