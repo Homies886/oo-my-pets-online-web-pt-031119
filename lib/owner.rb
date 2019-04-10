ac48fa1d8fa14489c7632947e7aefe40875ffdc0
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-10 16:51:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Owner
   
-  attr_accessor :name, :pets, :cats
+  attr_accessor :name, :pets
   attr_reader :species
   
   @@all = []
@@ -29,9 +29,8 @@ class Owner
   end  
   
   def buy_cat(name)
-    binding.pry
     new_cat = Cat.new(name)
-    @pets[cats] << new_cat
+    @pets[:cats] << new_cat
   end
   
   def buy_dog(name)
